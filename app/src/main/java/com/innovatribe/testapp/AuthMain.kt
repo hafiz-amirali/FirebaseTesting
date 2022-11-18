b46e94dbbd033cf845f8d3585d3e4425f743a703
Hafiz Amir Ali <hafizamir588@gmail.com>
2022-11-18 10:44:45 +0500

    UI created

---
+++ app/src/main/java/com/innovatribe/testapp/AuthMain.kt
@@ -0,0 +1,12 @@
+package com.innovatribe.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_main)
+
+    }
+}
